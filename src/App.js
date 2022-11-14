dbf2bc32de51c4381238660e98f27ab9aae7a987
amartyam12 <amartyam12@gmail.com>
2022-11-15 02:55:45 +0530

    last update

--- src/App.js
+++ src/App.js
@@ -21,29 +21,31 @@ function App() {
     let data=JSON.stringify({"text": input})
     console.log(data);       
   }
-  // function polarity(){
-  //   return console.log(result.result.polarity);
-  // }
-  // function type(){
-  //   return console.log(result.result.type);
-  // }
-  const polarity=result.result.polarity;
-  const type= result.result.type;
+  function polarity(){
+    return (result.result.polarity);
+  }
+  function type(){
+    return (result.result.type);
+  }
+  // var polarity="";
+  // polarity=result.result.polarity;
+  // console.log(polarity);
+  // var type= "";
+  // type=result.result.type;
  
 
   return (
     <>    <div className="App">
       <h1 style={{color:"blueviolet"}}>Paragraph Analysis</h1>
-      <input style={{height:"100px",width:"700px",fontSize:"30px",borderRadius:"10px"}}type="text" name="input" value={input}onChange={(e)=>{setInput(e.target.value)}}/>
+      <input style={{height:"100px",width:"700px",fontSize:"30px",borderRadius:"10px"}}type="text" name="input" value={input} onChange={(e)=>{setInput(e.target.value)}}/>
       <br/>
       <br/>
       <br/>
-      <input style={{fontSize:"20px",fontFamily:"cursive",padding:"8px 8px",borderRadius:"5px",backgroundColor:"blue",color:"white",border:"3px dotted white"}}type='button' onClick={submitHandler} value="Check for Sentiment"/>
+      <button style={{fontSize:"20px",fontFamily:"cursive",padding:"8px 8px",borderRadius:"5px",backgroundColor:"blue",color:"white",border:"3px dotted white"}}type='button' onClick={submitHandler}>Check for Sentiment</button>
+      <h1><span style={{border:"3px solid yellow", padding:"5px 5px", borderRadius:"10px",backgroundColor:"black",color:"red"}}>Polarity:{polarity()}</span></h1>
+      <h1><span style={{border:"3px solid yellow", padding:"5px 5px",borderRadius:"10px",backgroundColor:"black",color:"red"}}>Type: {type()}</span></h1>
       <br/>
       <br/>
-      <h1><span style={{border:"3px solid yellow", padding:"5px 5px", borderRadius:"10px",backgroundColor:"black",color:"red"}}>Polarity:{polarity}</span></h1>
-      <h1><span style={{border:"3px solid yellow", padding:"5px 5px",borderRadius:"10px",backgroundColor:"black",color:"red"}}>Type: {type}</span></h1>
-     
     </div>
     </>
 
