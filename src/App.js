8d76e66f1e1eb8ed54bf6d2f11c772990275543d
amartyam12 <110928659+amartyam12@users.noreply.github.com>
2026-02-17 15:56:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React,{ useState } from 'react';
 import Show from './Show';
 import './App.css';
 
-functi App() {
+function App() {
   const [input,setInput]=useState("");
   const [result,setResult]=useState([]);
  
